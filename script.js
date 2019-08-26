bf467ab9eb55f9132ba8e0f896e036b9142ff8df
typhain <typhaine.charrat@gmail.com>
2019-08-26 18:54:51 +0200

    exo_7

--- script.js
+++ script.js
@@ -44,3 +44,29 @@ var headerOnClick = function(){
 header.addEventListener("dblclick", headerOnClick)
 
 //Exercice 6
+function reduceCard() {
+  let cards = document.querySelectorAll(".card");
+
+  for (let index in cards) {
+    let cardImg = cards[index].querySelector(".card-img-top");
+    let textCard = cards[index].querySelector(".card-body p");
+    let viewBtn = cards[index].querySelector(".card .btn");
+
+    viewBtn.addEventListener("mouseover", function() {
+      if (viewBtn.classList.contains("mouse-over")) {
+        textCard.style.display = "";
+        cardImg.style.width = "";
+        cardImg.style.margin = "";
+      } else {
+        textCard.style.display = "none";
+        cardImg.style.width = "20%";
+        cardImg.style.margin = "auto";
+      }
+      viewBtn.classList.toggle("mouse-over");
+    });
+  }
+}
+
+reduceCard()
+
+//Exercice 7
